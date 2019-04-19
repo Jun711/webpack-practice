205789571010781219a1ac9891b926e14d49848a
Jun Goh <cygoh@umich.edu>
2019-04-18 19:27:43 -0700

    [feat] use css-loader’ and ‘style-loader’ to pack css - Inline styles in webpack output

--- practice1/src/components/App.js
+++ practice1/src/components/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './app-style.css';
+
 export class App extends Component {
  render() {
   return <p>Hi, I've been packed, and transpiled</p>
